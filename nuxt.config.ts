bf1afdab25a30f4ad0adb9f3083669209ab46632
angelorc <angelo@rcstart.it>
2025-10-04 14:56:37 +0200

    Add meta tags and Open Graph data for improved SEO and sharing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,27 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
 
+  app: {
+    head: {
+      title: 'Photon Marketcap',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'A marketcap dashboard for the Photon token' },
+        { property: 'og:title', content: 'Photon Marketcap' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://photonmarketcap.org' },
+        { property: 'og:image', content: 'https://photonmarketcap.org/og.png' },
+        { property: 'og:image:alt', content: 'Photon Marketcap' },
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:title', content: 'Photon Marketcap' },
+        { name: 'twitter:description', content: 'A marketcap dashboard for the Photon token' },
+        { name: 'twitter:image', content: 'https://photonmarketcap.org/og.png' },
+        { name: 'twitter:image:alt', content: 'Photon Marketcap' },
+
+      ],
+    }
+  },
+
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
   shadcn: {
     prefix: '',
